90b4ddb17235cf8d8dcf515129b20269669f24cc
thanhlongzzz <longvipzzz@gmail.com>
2021-11-01 00:53:39 +0700

    New release

--- lunarcrush/install_new_data.bat
+++ lunarcrush/install_new_data.bat
@@ -2,8 +2,9 @@ adb pull /sdcard/AutoTouchPro/data.csv data_rs_new.csv
 adb pull /sdcard/AutoTouchPro/data_device_1.csv data_device_1_rs.csv
 adb pull /sdcard/AutoTouchPro/data_device_2.csv data_device_2_rs.csv
 adb pull /sdcard/AutoTouchPro/data_device_3.csv data_device_3_rs.csv
-adb pull /sdcard/AutoTouchPro/data_rs.csv data_rs.csv
+adb pull /sdcard/AutoTouchPro/data_rs.csv data_rs_old.csv
 adb push data.csv /sdcard/AutoTouchPro
 adb push data_device_1.csv /sdcard/AutoTouchPro
 adb push data_device_2.csv /sdcard/AutoTouchPro
 adb push data_device_3.csv /sdcard/AutoTouchPro
+adb push data_rs.csv /sdcard/AutoTouchPro
