798774b746faa4548d95d553e4de031f600fc292
thanhlongzzz <longvipzzz@gmail.com>
2021-11-01 00:14:52 +0700

    New release

---
+++ lunarcrush/install.bat
@@ -0,0 +1,7 @@
+adb push register.atp /sdcard/AutoTouchPro
+adb push config.json /sdcard/AutoTouchPro
+adb push click.atp /sdcard/AutoTouchPro
+adb push nuoi.atp /sdcard/AutoTouchPro
+
+adb push coin.jpg /sdcard
+adb push moon.jpg /sdcard
